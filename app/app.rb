5bac5d610736e51038dbd39bfe1419cc76e72f3e
aoti <clanherb@gmail.com>
2011-09-04 22:32:23 +0800

    add support for archlinux(preliminary)

--- app/app.rb
+++ app/app.rb
@@ -18,14 +18,20 @@ class Nml < Sinatra::Base
   end
 
   def install(uuid, ipaddr, gateway, hostname, iface, baudrate, release)
-    if /^ubuntu/i =~ release
+    if /^archlinux/i =~ release
+      # LINUX  /arch/boot/i686/vmlinuz
+      # INITRD /arch/boot/i686/archiso.img
+      # APPEND archisobasedir=arch archisolabel=ARCH_201108
+      # /archlinux-201108-i686 /archlinux-201108-x86_64
       indent = ' ' * 4
-      head = "serial 0 #{baudrate}\ntimeout 50\nlabel pxeboot"
+      head = "serial 0 #{baudrate}\ntimeout 50\nlabel Unattended Installtion by NML"
       tail = "default #{release}/vesamenu.c32"
 
-      kernel = indent + "kernel %s/linux" % [release] 
+      kernel = indent + "kernel %s/vmlinuz" % [release] 
 
       configs = [
+        #"archisobasedir=arch",
+        "archisolabel=5b77c072-b18e-4f2a-8420-1588dff456bc",
         "autostep",
         "console-tools/archs=skip-config",
         "console-keymaps-at/keymap=us",
@@ -43,15 +49,51 @@ class Nml < Sinatra::Base
         "locale=en_US.UTF-8",
         "interface=#{iface}",
         "console=ttyS0,#{baudrate}n8",
+        "initrd=#{release}/archiso.img",
+      ]
+
+      append = indent + 'append ' + configs.join(' ')
+      [head, kernel, append, tail].join("\n") + "\n"
+    elsif /^ubuntu/i =~ release
+      indent = ' ' * 4
+      head = "serial 0 #{baudrate}\ntimeout 50\nlabel Unattended Installtion by NML"
+      tail = "default #{release}/vesamenu.c32"
+
+      kernel = indent + "kernel %s/linux" % [release] 
+
+      configs = [
+        "autostep",
+        "console-tools/archs=skip-config",
+        "vga=normal",
+        "netcfg/confirm_static=true",
+        "netcfg/disable_dhcp=true",
+        "netcfg/get_hostname=#{hostname}",
+        "netcfg/get_domain=.nml",
+        "netcfg/get_nameservers=%s" % [@@dns],
+        "netcfg/get_ipaddress=#{ipaddr}",
+        "netcfg/get_netmask=255.255.255.0",
+        "netcfg/get_gateway=#{gateway}",
+        "console-setup/ask_detect=false",
+        "console-setup/layoutcode=us",
+        "locale=en_US.UTF-8",
+        "console=ttyS0,#{baudrate}n8",
         "initrd=#{release}/initrd.gz",
         "auto url=http://%s/%s/preseed/#{uuid}" % [@@master, @@base]
       ]
+
+      if /^ubuntu-natty/i =~ release
+        configs << "netcfg/choose_interface=#{iface}"
+        configs << "keyboard-configuration/layoutcode=us"
+      else
+        configs << "interface=#{iface}"
+        configs << "console-keymaps-at/keymap=us"
+      end
 
       append = indent + 'append ' + configs.join(' ') + ' -- quiet'
       [head, kernel, append, tail].join("\n") + "\n"
     elsif /^centos/ =~ release
       indent = ' ' * 4
-      head = "serial 0 #{baudrate}\ntimeout 50\nlabel pxeboot"
+      head = "serial 0 #{baudrate}\ntimeout 50\nlabel Unattended Installtion by NML"
       tail = "default #{release}/vesamenu.c32"
 
       kernel = indent + "kernel %s/vmlinuz" % [release] 
@@ -80,8 +122,15 @@ class Nml < Sinatra::Base
     if uuid == '40f9d7c1-446c-b601-2911-001a6499e750'
       # 'centos-6.0-x86_64'
       # 'centos-6.0-i386'
-      #'centos-5.6-x86_64'
-        'centos-5.6-i386'
+      # 'centos-5.6-x86_64'
+      # 'centos-5.6-i386'
+      # 'ubuntu-lucid-i386'
+      # 'ubuntu-natty-i386'
+        'ubuntu-natty-amd64'
+    elsif uuid == '40c26bc0-486c-b601-f0a9-001a6499e680'
+      # 'archlinux-201108-i686'
+      # 'archlinux-201108-x86_64'
+        'ubuntu-natty-amd64'
     else
       'ubuntu-lucid-amd64'
     end
